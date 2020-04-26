bc7ab87e5d352588124053d3860b54de5678a8c8
Daniel Sun <danielsun12345@gmail.com>
2020-04-26 01:30:58 +0000

    Done.

--- db/migrate/20150915202821_add_column_to_cats.rb
+++ db/migrate/20150915202821_add_column_to_cats.rb
@@ -1,9 +1,15 @@
-class AddColumnToCats < ActiveRecord::Migration
-  def up
-    add_column :cats, :owner_id, :integer
-  end
+# class AddColumnToCats < ActiveRecord::Migration
+#   def up
+#     add_column :cats, :owner_id, :integer
+#   end
 
-  def down
-    remove_column :cats, :owner_id
+#   def down
+#     remove_column :cats, :owner_id
+#   end
+# end
+
+class AddColumnToCats < ActiveRecord::Migration
+  def change
+    add_column :cats, :owner_id, :integer
   end
 end
